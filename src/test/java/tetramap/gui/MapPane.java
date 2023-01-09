d041f62d3b523c41747252499256515cd77d8a81
avzhykov@gmail.com <20122012>
2023-01-09 09:21:50 +0600

    Добавлены тестовые классы для тестирования приложения

---
+++ src/test/java/tetramap/gui/MapPane.java
@@ -0,0 +1,12 @@
+package tetramap.gui;
+
+/**
+ * Панель с картой и панелями инструментов
+ */
+public interface MapPane {
+
+    /**
+     * Инициализация панели с картой
+     */
+    void initialize();
+}
