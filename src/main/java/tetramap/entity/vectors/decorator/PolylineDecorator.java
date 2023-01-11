0858eba87b2cc6019a28cb61074357506fe1a680
avzhykov@gmail.com <20122012>
2023-01-11 14:06:08 +0600

    Добавлены маркеры для начала и конца пути

--- src/main/java/tetramap/entity/vectors/decorator/PolylineDecorator.java
+++ src/main/java/tetramap/entity/vectors/decorator/PolylineDecorator.java
@@ -4,7 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.extern.log4j.Log4j2;
-import tetramap.entity.vectors.Path;
+import tetramap.entity.marker.Marker;
+import tetramap.entity.types.Icon;
+import tetramap.entity.types.LatLong;
+import tetramap.entity.vectors.Polyline;
+import tetramap.entity.vectors.structure.LatLongArray;
 import tetramap.gui.MapView;
 import tetramap.layer.Layer;
 import tetramap.type.TypeInstantiatesMap;
@@ -20,7 +24,7 @@ public class PolylineDecorator extends Layer {
     /**
      * Фигура для которой накладывается декоратор
      */
-    private Path path;
+    private Polyline polyline;
 
     /**
      * Смещение первого символа шаблона от начальной точки линии. По умолчанию: 0.
@@ -42,17 +46,28 @@ public class PolylineDecorator extends Layer {
      */
     private Symbol symbol;
 
-    public PolylineDecorator(Path path) {
-        this.path = path;
-        offset = 0;
-        endOffset = 0;
+    /**
+     * Маркеры для обозначения начала и конца пути
+     */
+    private Marker startMarker;
+    private Marker endMarker;
+
+    /**
+     * Иконка для маркеров
+     */
+    private Icon icon;
+
+    public PolylineDecorator(Polyline polyline) {
+        this.polyline = polyline;
+        offset = 50;
+        endOffset = 50;
         repeat = 100;
         symbol = new Symbol();
     }
 
     @Override
     public String toString() {
-        return path.getId() + "," + "{patterns: [{" +
+        return polyline.getId() + "," + "{patterns: [{" +
                 "offset: " + offset +
                 ", endOffset: " + endOffset +
                 ", repeat: " + repeat +
@@ -73,6 +88,23 @@ public class PolylineDecorator extends Layer {
 
         mapView.execScript(String.join("",this.getId(), " = L.", this.getTypeInstantiatesMap(),
                 "(", this.toString(), ").addTo(", mapView.getMap().getId() + ");"));
+
+        try {
+            icon = new Icon(getClass().getResource("/markerIcon/subscriber/marker_green.png").getPath());
+            icon.addTo(mapView);
+        } catch (NullPointerException e){
+            log.error("Иконка маркера маршрута не была загружена");
+        }
+
+        LatLongArray latLongArray = (LatLongArray)polyline.getLatLongs();
+        
+        LatLong latLongStart = latLongArray.get(0);
+        startMarker = new Marker(latLongStart, icon, latLongStart.toString());
+        startMarker.addTo(mapView);
+
+        LatLong latLongEnd = latLongArray.get(latLongArray.size() - 1);
+        endMarker = new Marker(latLongEnd, icon, latLongEnd.toString());
+        endMarker.addTo(mapView);
     }
 
     @Override
@@ -80,8 +112,19 @@ public class PolylineDecorator extends Layer {
         log.info("Удаление с карты слоя layer: {}", this.getId());
         getMapView().execScript(this.getId() + ".remove();");
 
+        startMarker.remove();
+        endMarker.remove();
+
         log.info("Обновляем значение layer: {}", this.getId());
         getMapView().execScript(String.join("",this.getId(), " = L.", this.getTypeInstantiatesMap(),
                 "(", this.toString(), ").addTo(", getMapView().getMap().getId() + ");"));
+
+        LatLong latLongStart = ((LatLongArray)polyline.getLatLongs()).get(0);
+        startMarker = new Marker(latLongStart, icon, latLongStart.toString());
+        startMarker.addTo(getMapView());
+
+        LatLong latLongEnd = ((LatLongArray)polyline.getLatLongs()).get(0);
+        endMarker = new Marker(latLongEnd, icon, latLongEnd.toString());
+        endMarker.addTo(getMapView());
     }
 }
